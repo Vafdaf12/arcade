de5e33f10a45d499aec0273c1c21264ce8a6b72a
vafdaf12 <vafdaf11@gmail.com>
2023-01-22 22:48:17 +0200

    Add SetPixel and GetIndex convenience methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "SDL_error.h"
 #include "SDL_events.h"
+#include "SDL_surface.h"
 #include "SDL_video.h"
 #include <SDL.h>
 #include <iostream>
 
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
+
+void SetPixel(SDL_Surface* pSurface, uint32_t color, int x, int y);
+size_t GetIndex(SDL_Surface* pSurface, int r, int c);
+
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO)) {
@@ -25,6 +30,13 @@ int main() {
         return 1;
     }
 
+    SDL_Surface* pWindowSurface = SDL_GetWindowSurface(pWindow);
+
+    uint32_t color = 0xFF0000;
+
+    SetPixel(pWindowSurface, color, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    SDL_UpdateWindowSurface(pWindow);
+
     SDL_Event event;
     bool running = true;
     while (running) {
@@ -38,4 +50,18 @@ int main() {
     SDL_Quit();
 
     std::cout << "Hello World\n";
+}
+
+void SetPixel(SDL_Surface* pSurface, uint32_t color, int x, int y) {
+    SDL_LockSurface(pSurface);
+
+    uint32_t* pixels = static_cast<uint32_t*>(pSurface->pixels);
+    size_t i = GetIndex(pSurface, y, x);
+    pixels[i] = color;
+
+    SDL_UnlockSurface(pSurface);
+
+}
+size_t GetIndex(SDL_Surface* pSurface, int r, int c) {
+    return r * pSurface->w + c;
 }
