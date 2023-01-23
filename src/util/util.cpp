6b9dde7ea043c2dcc5938e5c80878e790b35345d
vafdaf12 <vafdaf11@gmail.com>
2023-01-24 00:33:33 +0200

    change: move float threshold to header for use

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -2,9 +2,7 @@
 
 #include <cmath>
 
-#define FLOAT_THRESHOLD 1e-4
-
 bool isEqual(float x, float y)
 {
-    return fabsf(x - y) < FLOAT_THRESHOLD;
+    return fabsf(x - y) < EPSILON;
 }
--- src/util/util.h
+++ src/util/util.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#define EPSILON 1e-4
+
 bool isEqual(float x, float y);
