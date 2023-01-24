ef22d8ac2819d7445fc7246579b5c8cca0f937ef
vafdaf12 <vafdaf11@gmail.com>
2023-01-25 00:08:41 +0200

    feat: add Circle shape

---
+++ src/shapes/Circle.cpp
@@ -0,0 +1,36 @@
+#include "Circle.h"
+
+#include <cmath>
+
+const float CIRCLE_DIST = 2.0f;
+
+
+Circle::Circle() : Circle(Vector2::ZERO, 0) {}
+Circle::Circle(const Vector2& center, float radius) {
+    m_points.push_back(center);
+    m_radius = radius;
+}
+
+bool Circle::intersects(const Circle& circle) const { 
+    float threshold = circle.m_radius + m_radius;
+    return m_points[0].distance(circle.getCenterPoint()) <= threshold;
+}
+bool Circle::containsPoint(const Vector2& point) const { 
+    return getCenterPoint().distance(point) <= m_radius;
+
+}
+std::vector<Vector2> Circle::getPoints() const { 
+    static unsigned int NUM_SEGMENTS = 30;
+
+    std::vector<Vector2> points;
+    float da = 2 * M_PI / NUM_SEGMENTS;
+
+    Vector2 delta = Vector2::NEG_Y * m_radius;
+
+    for(int i = 0; i < NUM_SEGMENTS; i++) {
+        points.push_back(getCenterPoint() + delta);
+        delta = delta.rotate(da);
+    }
+
+    return points;
+}
---
+++ src/shapes/Circle.h
@@ -0,0 +1,23 @@
+#include "Shape.h"
+#include <vector>
+
+class Circle : public Shape {
+public:
+    Circle();
+    Circle(const Vector2& center, float radius);
+    
+    inline Vector2 getCenterPoint() const override { return m_points[0]; }
+    virtual std::vector<Vector2> getPoints() const override;
+
+    inline float getRadius() const { return m_radius; }
+    inline void setRadius(float r) { m_radius = r; }
+
+    inline void moveTo(const Vector2& newPos) { m_points[0] = newPos; }
+
+    bool intersects(const Circle& circle) const;
+    bool containsPoint(const Vector2& point) const;
+
+
+private:
+    float m_radius;
+};
