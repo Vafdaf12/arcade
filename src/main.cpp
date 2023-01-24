4318a81d1ec6c68eb4aa3dbc944671546fd440d6
vafdaf12 <vafdaf11@gmail.com>
2023-01-24 16:51:53 +0200

    feat: add line drawing example to main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include "graphics/Color.h"
 #include "graphics/Screen.h"
 #include "graphics/ScreenBuffer.h"
+#include "shapes/Line2D.h"
 
 #include <SDL.h>
 #include <iostream>
 
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
-const int MAGNIFICATION = 4;
+const int MAGNIFICATION = 3;
 
 int main() {
     Screen screen;
     screen.init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
 
     screen.draw(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, Color::Orange());
+
+    Line2D line({0, 0}, {SCREEN_WIDTH, SCREEN_HEIGHT});
+    screen.draw(line, Color::White());
+
+
     screen.swapBuffers();
 
     SDL_Event event;
