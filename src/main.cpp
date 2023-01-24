3aff8470ef24a4b4c965e6df1ae9372be815ebb1
vafdaf12 <vafdaf11@gmail.com>
2023-01-25 00:13:34 +0200

    feat: add example code for drawing shapes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "graphics/Color.h"
 #include "graphics/Screen.h"
 #include "graphics/ScreenBuffer.h"
+#include "shapes/AARectangle.h"
 #include "shapes/Line2D.h"
+#include "shapes/Triangle.h"
+#include "shapes/Circle.h"
 
 #include <SDL.h>
 #include <iostream>
@@ -19,6 +22,14 @@ int main() {
     Line2D line({0, 0}, {SCREEN_WIDTH, SCREEN_HEIGHT});
     screen.draw(line, Color::White());
 
+    Triangle triangle({2, 2}, {50, 30}, {20, 60});
+    screen.draw(triangle, Color::Cyan());
+
+    Circle circle({100, 100}, 30);
+    screen.draw(circle, Color::Red());
+
+    AARectangle rect({100, 20}, 30, 20);
+    screen.draw(rect, Color::Green());
 
     screen.swapBuffers();
 
