74e8c27e12746d838987a64065325b540e30830a
vafdaf12 <vafdaf11@gmail.com>
2023-01-24 20:01:32 +0200

    feat: add axis-aligned rectangle shape

---
+++ src/shapes/AARectangle.cpp
@@ -0,0 +1,62 @@
+#include "AARectangle.h"
+
+#include <numeric>
+
+AARectangle::AARectangle() : AARectangle(Vector2::ZERO, Vector2::ZERO) {}
+
+AARectangle::AARectangle(
+    const Vector2& topLeft, unsigned int width, unsigned int height) {
+    m_points.push_back(topLeft);
+    Vector2 delta(width - 1, height - 1);
+
+    m_points.push_back(topLeft + delta);
+}
+
+AARectangle::AARectangle(const Vector2& topLeft, const Vector2& bottomRight) {
+    m_points.push_back(topLeft);
+    m_points.push_back(bottomRight);
+}
+
+Vector2 AARectangle::getCenterPoint() const {
+    Vector2 total =
+        std::accumulate(m_points.begin(), m_points.end(), Vector2::ZERO);
+    return total / m_points.size();
+}
+
+std::vector<Vector2> AARectangle::getPoints() const {
+    std::vector<Vector2> points;
+    points.push_back(m_points[0]);
+    points.push_back(m_points[0] + Vector2::POS_X * getWidth());
+    points.push_back(m_points[1]);
+    points.push_back(m_points[0] + Vector2::POS_Y * getHeight());
+
+    return points;
+}
+
+void AARectangle::moveTo(const Vector2& newPos) {
+    Vector2 delta(getWidth(), getHeight());
+    m_points[0] = newPos;
+    m_points[1] = newPos + delta;
+}
+
+bool AARectangle::intersects(const AARectangle& other) const {
+    if (m_points[1].x < other.m_points[0].x) return false;
+    if (m_points[0].x > other.m_points[1].x) return false;
+
+    if (m_points[1].y < other.m_points[0].y) return false;
+    if (m_points[0].y > other.m_points[1].y) return false;
+
+    return true;
+}
+bool AARectangle::containsPoint(const Vector2& point) const {
+    if (point.x < m_points[0].x) return false;
+    if (point.y < m_points[0].y) return false;
+
+    if (point.x > m_points[1].x) return false;
+    if (point.y > m_points[1].y) return false;
+
+    return true;
+}
+AARectangle AARectangle::inset(const AARectangle& rect, const Vector2& amt) {
+    return AARectangle(rect.getTopLeft() + amt, rect.getBottomRight() - amt);
+}
---
+++ src/shapes/AARectangle.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Shape.h"
+#include "../util/Vector2.h"
+
+class AARectangle : public Shape {
+public:
+    AARectangle();
+    AARectangle(const Vector2& topLeft, unsigned int width, unsigned int height);
+    AARectangle(const Vector2& topLeft, const Vector2& bottomRight);
+
+    virtual Vector2 getCenterPoint() const override;
+    virtual std::vector<Vector2> getPoints() const override;
+
+    inline void setTopLeft(const Vector2& topLeft) { m_points[0] = topLeft; }
+    inline void setBottomRight(const Vector2& bottomRight) { m_points[1] = bottomRight; }
+
+    inline Vector2 getTopLeft() const { return m_points[0]; }
+    inline Vector2 getBottomRight() const { return m_points[1]; }
+
+    inline float getWidth() const { return m_points[1].x - m_points[0].x + 1; }
+    inline float getHeight() const { return m_points[1].y - m_points[0].y + 1; }
+
+    void moveTo(const Vector2& newPos);
+    bool intersects(const AARectangle& other) const;
+    bool containsPoint(const Vector2& point) const;
+    static AARectangle inset(const AARectangle& rect, const Vector2& insets);
+
+
+};
