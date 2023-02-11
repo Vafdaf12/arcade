3f484e231603955840cd5aa687a6f146d20d3d0e
vafdaf12 <vafdaf11@gmail.com>
2023-02-11 22:47:22 +0200

    build: add static + windows build support

---
+++ Win32MinGW.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-win32)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-win32)
+
+set(BUILD_SHARED_LIBS OFF)
+set(CMAKE_EXE_LINKER_FLAGS "-static")
+
+
